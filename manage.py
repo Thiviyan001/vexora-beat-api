04e47f0f9d48ca532807d0dd7ac847927d0e5137
Thiviyan001 <vimalgoretti@gmail.com>
2026-09-22 20:19:02 +0530

    Create Django backend for BEAT

---
+++ manage.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vexora.settings")
+
+from django.core.management import execute_from_command_line
+
+if __name__ == "__main__":
+    execute_from_command_line(sys.argv)
